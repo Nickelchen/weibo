d7fff402f343ba72ae01da0c16099864326341d5
Nickelchen <1332446313@qq.com>
2021-10-22 16:02:42 +0800

    登录时记住我

--- resources/views/sessions/create.blade.php
+++ resources/views/sessions/create.blade.php
@@ -12,8 +12,17 @@
         <input type="text" name="email" class="form-control"
             value="{{ old('email') }}"> 
         </div>
-        <div class="form-group"> <label for="password">密码：</label> <input type="password" name="password"
-            class="form-control" value="{{ old('password') }}"> </div> <button type="submit"
+        <div class="form-group"> <label for="password">密码：</label> 
+        <input type="password" name="password"
+            class="form-control" value="{{ old('password') }}"> 
+            </div> 
+            <div class="form-group"> 
+            <div class="form-check"> 
+            <input type="checkbox" class="form-check-input" name="remember" id="exampleCheck1"> 
+            <label class="form-check-label" for="exampleCheck1">记住我 </label> 
+            </div> 
+            </div>
+            <button type="submit"
           class="btn btn-primary">登录</button>
       </form>
       <hr>
