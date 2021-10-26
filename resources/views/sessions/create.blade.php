97ca960b0dfe6796e728c8bb2149e7b114d76588
Nickelchen <1332446313@qq.com>
2021-10-26 23:06:19 +0800

    发送找回密码连接

--- resources/views/sessions/create.blade.php
+++ resources/views/sessions/create.blade.php
@@ -8,22 +8,20 @@
     </div>
     <div class="card-body"> @include('shared._errors') <form method="POST" action="{{ route('login') }}">
         {{ csrf_field() }}
-        <div class="form-group"> <label for="email">邮箱：</label> 
-        <input type="text" name="email" class="form-control"
-            value="{{ old('email') }}"> 
+        <div class="form-group"> <label for="email">邮箱：</label>
+        <input type="text" name="email" class="form-control" value="{{ old('email') }}">
         </div>
-        <div class="form-group"> <label for="password">密码：</label> 
+        <div class="form-group"> <label for="password">密码：（<a href="{{ route('password.request') }}">忘记密码</a>）</label>
         <input type="password" name="password"
-            class="form-control" value="{{ old('password') }}"> 
-            </div> 
-            <div class="form-group"> 
-            <div class="form-check"> 
-            <input type="checkbox" class="form-check-input" name="remember" id="exampleCheck1"> 
-            <label class="form-check-label" for="exampleCheck1">记住我 </label> 
-            </div> 
+            class="form-control" value="{{ old('password') }}">
             </div>
-            <button type="submit"
-          class="btn btn-primary">登录</button>
+            <div class="form-group">
+            <div class="form-check">
+            <input type="checkbox" class="form-check-input" name="remember" id="exampleCheck1">
+            <label class="form-check-label" for="exampleCheck1">记住我 </label>
+            </div>
+            </div>
+            <button type="submit" class="btn btn-primary">登录</button>
       </form>
       <hr>
       <p>还没账号？<a href="{{ route('signup') }}">现在注册！</a></p>
