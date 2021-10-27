f0cb9c92e1b0798d05fa9a9219996ecfb31ae45d
Nickelchen <1332446313@qq.com>
2021-10-27 09:17:58 +0800

    发布微博

--- app/Models/Status.php
+++ app/Models/Status.php
@@ -1,14 +1,16 @@
-<?php 
-namespace App\Models; 
+<?php
+namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory; 
-use Illuminate\Database\Eloquent\Model; 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-class Status extends Model { 
-    use HasFactory; 
+class Status extends Model {
+    use HasFactory;
     
-    public function user() 
-    { 
-        return $this->belongsTo(User::class); 
-    } 
+    protected $fillable = ['content'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
