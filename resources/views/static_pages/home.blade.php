08e744337745673c5f4583daee0aad07919a59b5
Nickelchen <1332446313@qq.com>
2021-10-27 10:07:59 +0800

    社交统计信息

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -13,7 +13,10 @@
 <aside class="col-md-4"> 
 <section class="user_info"> 
 @include('shared._user_info', ['user' => Auth::user()]) 
-</section> 
+</section>
+<section class="stats mt-2"> 
+@include('shared._stats', ['user' => Auth::user()]) 
+</section>
 </aside> 
 </div> 
 @else 
